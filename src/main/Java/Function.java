b1ebec7e3ccebd2db5d0d1e267cd94a0367dbd05
kirjanx <kiryl_parkhomenka@epam.com>
2017-01-04 17:13:21 +0300

    Function class was created. Methods to create were detected.

---
+++ src/main/Java/Function.java
@@ -0,0 +1,43 @@
+public class Function {
+
+    private String filePath;
+    private int numberOfStrings;
+
+    public void readFile (String filePath, int numberOfStrings) {
+        this.filePath = filePath;
+        this.numberOfStrings = numberOfStrings;
+
+    }
+
+    public void readFile (String filePath) {
+        this.filePath = filePath;
+        this.numberOfStrings = 10;
+
+    }
+
+    public void cutStringsFromFile () {
+
+    }
+
+    public void writeCutStringsInResultFile () {
+
+    }
+
+    public void saveInitialFileWithoutCutStrings () {
+
+    }
+
+    public String returnPathToInitialFile () {
+
+        return ;
+
+    }
+
+
+
+
+
+
+
+
+}
